478688ba9fb9c81eeb588e717ba61a9ba85f5cc5
Gleison de Almeida <gleisonsubzerokz@gmail.com>
2022-05-28 12:38:56 -0300

    :sparkles: feat: build types and resources dynamically

--- src/enums/type.enum.ts
+++ src/enums/type.enum.ts
@@ -1,13 +1,16 @@
 export enum eResourceType {
   COMPONENT = "component",
   DIRECTIVE = "directive",
+  SERVICE = "service",
   PIPE = "pipe",
 }
 
 export enum eComponentType {
+  COMMON = "common",
+  DIALOG = "dialog",
   PAGE = "page",
   WIDGET = "widget",
 }
 
-export const ALL_RESOURCE_TYPES = [eResourceType.COMPONENT, eResourceType.DIRECTIVE, eResourceType.PIPE];
-export const ALL_COMPONENT_TYPES = [eComponentType.PAGE, eComponentType.WIDGET];
+export const ALL_RESOURCE_TYPES = Object.values(eResourceType);
+export const ALL_COMPONENT_TYPES = Object.values(eComponentType);
